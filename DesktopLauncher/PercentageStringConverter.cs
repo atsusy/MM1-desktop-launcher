58313539a83cac53af17c65239c629bf97c51df2
Atsushi Kataoka <atsushi.kataoka@msmc.jp>
2017-06-25 12:12:16 +0900

    Add window's opacity option #10

---
+++ DesktopLauncher/PercentageStringConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace DesktopLauncher
+{
+    public class PercentageStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            decimal v = 0m;
+
+            decimal.TryParse(value.ToString(), out v);
+            return (int)(v * 100.0m) + "%";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            decimal v = 0m;
+
+            decimal.TryParse(value.ToString().TrimEnd(new char[] { '%' }), out v);
+
+            return (v / 100.0m);
+        }
+    }
+}
