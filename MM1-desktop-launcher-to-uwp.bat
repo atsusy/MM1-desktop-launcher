eac19865e4fa64f6cc6e16f0f9cb289cedaff862
Atsushi Kataoka <atsushi.kataoka@msmc.jp>
2017-10-17 09:31:43 +0900

    Add DesktopAppConverter command

---
+++ MM1-desktop-launcher-to-uwp.bat
@@ -0,0 +1,13 @@
+DesktopAppConverter.exe ^
+-Installer C:\Release\ ^
+-AppExecutable MM1DesktopLauncher.exe ^
+-Destination C:\Output\MM1DesktopLauncher ^
+-PackageName "MM1DesktopLauncher" ^
+-Publisher "CN=DEE76187-35BE-4316-B0BC-17CB2F7F0E04" ^
+-Version 0.6.0.0 ^
+-MakeAppx ^
+-Sign ^
+-Verbose ^
+-PackagePublisherDisplayName "MARSHMALLOW MACHINE Inc." ^
+-AppDisplayName "MM1 desktop launcher" ^
+-PackageDisplayName "MM1 desktop launcher"
